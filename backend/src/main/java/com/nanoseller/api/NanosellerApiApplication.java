96d3a82a2998b4170589f545c3f40a7ac032046a
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-12-30 17:00:34 -0300

    upload dos arquivos iniciais do backend com spring boot em java 21 e maven

---
+++ backend/src/main/java/com/nanoseller/api/NanosellerApiApplication.java
@@ -0,0 +1,13 @@
+package com.nanoseller.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanosellerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanosellerApiApplication.class, args);
+	}
+
+}
